44a54a93f22b9387e825052980edbdc060e7390a
NicoJC25 <nejuez@misena.edu.co>
2024-08-01 15:03:22 -0500

    SASS and Gulp Implementation

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+import {src, dest, watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+// export function hola( done ){
+//     console.log('Hola desde Gulpfile.js');
+
+//     done();
+// }
+
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('dist/css') );
+
+    done();
+}
+
+export function dev() {
+    watch('src/scss/app.scss', css);
+}
